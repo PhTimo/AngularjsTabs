42cb2d72bc86f4a98e888a1fae81af4eb395679c
Philipp <timoshenkophilipp@gmail.com>
2015-11-28 23:16:07 +0300

    First push

---
+++ example.js
@@ -0,0 +1,19 @@
+﻿angular.module('tabs.demo', ['ui.bootstrap']);
+angular.module('tabs.demo').controller('tabsCtrl', function ($scope) {
+  $scope.tabs = [
+    { title:'Tab1 ', content:'/tab1.html' },
+    { title:'Tab2', content:'/tab2.html'},
+    { title:'Tab3', content:'/tab3.html'}
+  ];
+
+
+});
+
+
+angular.module('tabs.demo').controller('newCtrl', function ($scope) {
+    $scope.cards = [
+      { title: '11', content: '/tab1.html' },
+      { title: '2', content: '/tab2.html' },
+      { title: '3', content: '/tab3.html' }
+    ];
+});
