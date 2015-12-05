2241b3cc3f43773af20c6f20bf70e4d650389dcc
PhTimo <timoshenkophilipp@gmail.com>
2015-12-05 23:03:01 +0300

    Update example.js

--- example.js
+++ example.js
@@ -27,10 +27,7 @@ angular.module('tabsdemo').controller('newCtrl', function ($scope) {
 });
 
 
-angular.module('tabsdemo', []).
-  run(function() {
-    FastClick.attach(document.body);
-  });
+
 
     
     var domElement = document
